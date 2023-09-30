9874b6ba563ab74b6be6f0cd6ee0db790719e6da
ciromelody <ciromelody@gmail.com>
2023-09-30 16:49:47 +0200

    Game Over ritorna alla activity principale oppure esce

--- app/src/main/java/edu/ciromelody/gamescheleton/numerouno/GameOver.java
+++ app/src/main/java/edu/ciromelody/gamescheleton/numerouno/GameOver.java
@@ -2,8 +2,11 @@ package edu.ciromelody.gamescheleton.numerouno;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
 
+import edu.ciromelody.gamescheleton.MainActivity;
 import edu.ciromelody.gamescheleton.R;
 
 public class GameOver extends AppCompatActivity {
@@ -12,5 +15,18 @@ public class GameOver extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_game_over);
+    }
+
+    public void restart(View view) {
+
+        Intent intent = new Intent(GameOver.this, MainActivity.class);
+        startActivity(intent);
+        finish();
+        return;
+    }
+
+    public void exit(View view) {
+        finish();
+        return;
     }
 }
