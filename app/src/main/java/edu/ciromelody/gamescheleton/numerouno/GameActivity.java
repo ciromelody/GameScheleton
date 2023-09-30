c9196cf997dc1c2e534e066269276ad3ea3a76d7
ciromelody <ciromelody@gmail.com>
2023-09-30 15:39:12 +0200

    Initial commit

---
+++ app/src/main/java/edu/ciromelody/gamescheleton/numerouno/GameActivity.java
@@ -0,0 +1,16 @@
+package edu.ciromelody.gamescheleton.numerouno;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import edu.ciromelody.gamescheleton.R;
+
+public class GameActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_game);
+    }
+}
