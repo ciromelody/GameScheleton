c968fc25dbcda43f1a1397a144cbb7858f81ae94
ciromelody <ciromelody@gmail.com>
2023-10-06 17:39:47 +0200

    arrow con croce
    aggiunta una croce rossa

--- app/src/main/java/edu/ciromelody/gamescheleton/utility/BitmapUtility.java
+++ app/src/main/java/edu/ciromelody/gamescheleton/utility/BitmapUtility.java
@@ -2,6 +2,12 @@ package edu.ciromelody.gamescheleton.utility;
 
 import android.graphics.Bitmap;
 import android.graphics.Canvas;
+import android.graphics.Color;
+import android.graphics.Paint;
+import android.graphics.Path;
+
+
+import edu.ciromelody.gamescheleton.numerouno.Arrow;
 
 public class BitmapUtility {
     Canvas canvas;
@@ -18,5 +24,26 @@ public class BitmapUtility {
 
 
         return bitmap;
+    }
+    public static Path disegnaCroce(int oggetto_posizioneX, int oggetto_posizioneY, Arrow arrow,Paint paintsbagliato){
+
+
+       Path mPath=new Path();
+        mPath.reset();
+        mPath.moveTo(oggetto_posizioneX, oggetto_posizioneY);
+
+        mPath.lineTo(oggetto_posizioneX+(arrow.getBitmap().getWidth()), oggetto_posizioneY+(arrow.getBitmap().getHeight()));
+        mPath.moveTo(oggetto_posizioneX, oggetto_posizioneY+(arrow.getBitmap().getHeight()));
+        mPath.lineTo(oggetto_posizioneX+(arrow.getBitmap().getWidth()), oggetto_posizioneY);
+      /*  for (int i = 50; i < 100; i++) {
+            mPath.moveTo(i, i-1);
+            mPath.lineTo(i, i);
+        }*/
+        mPath.close();
+        paintsbagliato.setStrokeWidth(30);
+        paintsbagliato.setPathEffect(null);
+        paintsbagliato.setColor(Color.RED);
+        paintsbagliato.setStyle(Paint.Style.STROKE);
+        return mPath;
     }
 }
